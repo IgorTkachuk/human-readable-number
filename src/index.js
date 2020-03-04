e03e68a41095d7035650d6aa313486b6988b79c9
Igor.Tkachuk <I.Tka4yk@gmail.com>
2020-03-04 13:10:53 +0200

    feat: implement toReadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const first19 = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen"
+    ];
+
+    tens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety"
+    ];
+
+    if (number < 20) return first19[number];
+
+    let result = "";
+
+    const hundreds = Math.trunc(number / 100);
+    const dozens = Math.trunc((number - hundreds * 100) / 10);
+    const rest = number - hundreds * 100 - dozens * 10;
+
+    if (hundreds) result += first19[hundreds] + " hundred";
+    if (hundreds && dozens) result += " ";
+    if (dozens > 0 && dozens < 2) {
+        result += first19[number - hundreds * 100];
+        return result;
+    } else {
+        result += tens[dozens];
+    }
+    if (rest && (hundreds || dozens)) result += " ";
+    if (rest) result += first19[rest];
+
+    return result;
+};
